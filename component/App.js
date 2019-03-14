00eb128b7a1157b4e9c115de7878d3db64701624
Shio <shio@Shio-3.local>
2019-03-14 17:44:45 +0700

    Edit App Components

---
+++ component/App.js
@@ -0,0 +1,20 @@
+// import library
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            //componet about react native
+            <View>
+                <Text>Hello Shio</Text>
+            </View>
+        );
+    }
+}
+
+// export
+export default App;
+
+
